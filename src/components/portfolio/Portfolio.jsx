0255a8cfe6d5ba4922000dd64a5fb9a587270757
YONDAROUGE <119593024+YONDAROUGE@users.noreply.github.com>
2023-06-24 10:09:55 +0100

    COMMIT3

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,105 +1,45 @@
-import React, { useEffect, useState } from 'react'; 
+import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpeg'
-import IMG2 from '../../assets/portfolio2.jpeg'
-import IMG3 from '../../assets/portfolio3.jpeg'
-import IMG4 from '../../assets/portfolio4.png'
-import IMG5 from '../../assets/portfolio5.jpeg'
-import IMG6 from '../../assets/portfolio6.jpeg'
+import "./portfolio.css";
 
-const data = [
-    {
-        id: 1,
-        image: IMG1,
-        title: 'Data Analysis Research Sample',
-        github: 'https://github.com',
-        demo: 'https://dribbble.com/Alien_pixels'
-    },
-    {
-        id: 2,
-        image: IMG2,
-        title: 'Website sample',
-        github: 'https://github.com',
-        demo: 'https://dribbble.com/Alien_pixels'
-    },
-    {
-        id: 3,
-        image: IMG3,
-        title: 'Figma dashboard UI kit for data design web apps',
-        github: 'https://github.com',
-        demo: 'https://dribbble.com/Alien_pixels'
-    },
-    {
-        id: 4,
-        image: IMG4,
-        title: 'Maintaining tasks and tracking progress',
-        github: 'https://github.com',
-        demo: 'https://dribbble.com/Alien_pixels'
-    },
-    {
-        id: 5,
-        image: IMG5,
-        title: 'School Management System',
-        github: 'https://github.com',
-        demo: 'https://dribbble.com/Alien_pixels'
-    },
-    {
-        id: 6,
-        image: IMG6,
-        title: 'Charts template & infographics in Figma ',
-        github: 'https://github.com',
-        demo: 'https://dribbble.com/Alien_pixels'
-    },
-]
+export const Portfolio = () => {
 
-const Portfolio = () => {
+    const [data, setData] = useState([]);
 
-  const[data , setData] = useState([]);
-  useEffect (()  => {
-    axios.get("https://brick-red-puffer-vest.cyclic.app/api/portfolio").then(response => {
-      const formattedData = response.data.data.map(item => ({
-        id:item._id,
-        image: item.image,
-        title:item.github,
-        demo:item.demo
-    }));
-    setData(formattedData)
-    }).catch(error=> {
-         console.log("Error fteching the portfolio data",error);
+    useEffect(() => {
+        axios.get("https://brick-red-puffer-vest.cyclic.app/api/portfolio").then(response => {
+            const formattedData = response.data.data.map(item => ({
+                id: item._id,
+                image: item.image,
+                title: item.title,
+                github: item.github,
+                demo: item.demo
+            }));
+            setData(formattedData);
+        }).catch(error => {
+            console.log("Error fetching portfolio data:", error);
+        })
+    }, [])
 
-    } )
-    
-    
-  },  []);
     return (
-        <section id='portfolio'>
+        <section id="portfolio">
             <h5>My Recent Work</h5>
             <h2>Portfolio</h2>
-
             <div className="container portfolio__container">
-                {
-
-
-                    data.map(({ id, image, title, github, demo }) => (
-                            <article key={id} className='portfolio__item'>
-                                <div className="portfolio__item-image">
-                                    <img src={image} alt={title} />
-                                </div>
-                                <h3>{title}</h3>
-                                <div className="portfolio__item-cta">
-                                    <a href={github} className='btn' target='_blank'>GitHub</a>
-                                    <a href={demo} target='_blank' className='btn btn-primary'>Live Demo</a>
-                                </div>
-                            </article>
-                        ))
-
-
-
-               }
+                {data.map(({ id, image, title, github, demo }) => (
+                    <article key={id} className="portfolio__item">
+                        <div className="portfolio__item__image">
+                            <img src={image} alt={title} className="portImage"/>
+                        </div>
+                        <h3>{title}</h3>
+                        <div className="portfolio__item__cta">
+                            <a href={github} className="btn" target="_blank" rel="nopoener noreferrer">GitHub</a>
+                            <a href={demo} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Live Demo</a>
+                        </div>
+                    </article>
+                ))}
             </div>
         </section>
-    )
-}
-
-export default Portfolio
+    );
+};
+export default Portfolio;
--- src/components/testimonial/testimonial.jsx
+++ src/components/testimonial/testimonial.jsx
@@ -1,91 +1,52 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import './testimonial.css'
-import AVTR1 from '../../assets/changer.png'
-import AVTR2 from '../../assets/img2.png'
-import AVTR3 from '../../assets/img9.png'
-import AVTR4 from '../../assets/changer.png'
+import "./testimonial.css";
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
+const Testimonial = () => {
+    const [data, setData] = useState([]);
 
-// import Swiper core and required modules
+    useEffect(() => {
+        axios.get("https://brick-red-puffer-vest.cyclic.app/api/testimonial").then(response => {
+            const formattedData = response.data.data.map(item => ({
+                id: item._id,
+                avatar: item.avatar,
+                nam: item.name,
+                review: item.review
+            }));
+            setData(formattedData);
+        }).catch(error => {
+            console.log("Error fetching testimonials data:", error);
+        })
+    }, [])
 
-import { Navigation } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-                                                                         
-// const data = [
-//     {
-//         avatar: AVTR1,
-//         name: 'Michelle',
-//         review: 'I was so happy with the data analysis NICCI conducted when I carried out my survey. No regrets.'
-//     },
-//     {
-//         avatar: AVTR2,
-//         name: 'Kelton',
-//         review: 'NICCI knows exactly what he is doing he is the absolute best.'
-//     },
-//     {
-//         avatar: AVTR3,
-//         name: 'Jake Allen',
-//         review: 'I was very happy with the work that NICCI did on my website. He was able to take my vision and turn it into a reality. The website is exactly what I wanted and it looks great.'
-//     },
-//     {
-//         avatar: AVTR4,
-//         name: 'Hacynth',
-//         review: 'NICCI is very easy to work with. He was always responsive to my feedback and was always willing to make changes to the content until I was satisfied'  
-//     }
-// ]
-
-const Testimonials = () => {
-    const[data, setData] = useState([]);
-useEffect(() => {
-    axios
-    .get("https://brick-red-puffer-vest.cyclic.app/api/testimonial")
-    .then(res=> {
-        const formmatedData = res.data.data.map(item => ({
-            id:item._id,
-            avatar:item.avatar,
-            name:item.name,
-            review:item.review,
-      
-        }));
-console.log(`Here i am: ${formmatedData}`);
-setData(formmatedData);
-} )
-.catch(error=> console.error(error));
-},[])
     return(
-
-        
-        <section id='testimonial'>
-            <h5>Review from clients</h5>
+        <section id="testimonials">
+            <h5>Review from Clients</h5>
             <h2>Testimonials</h2>
+            <Swiper className="container testimonials__container"
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true }} >
 
-            <Swiper className="container testimonials__container" // install Swiper modules
-                modules={[Navigation]}
-                spaceBetween={40}
-                slidesPerView={1}
-                navigation>
-                {
-                    data.map(({ avatar, name, review }, index) => {
-                        return (
-                            <SwiperSlide key={index} className='testimonial'>
-                                <div className='client__avatar'>
-                                    <img src={avatar} />
-                                </div>
-                                <h5 className='client__name'>{name}</h5>
-                                <small className='client__review'>{review}</small>
-                            </SwiperSlide>
-                        )
-                    })
-                }
+            {
+                data.map(({id, avatar, name, review}) => (
+                    <SwiperSlide key={id} className="testimonial">
+                        <div className="client__avatar">
+                            <img src={avatar} alt={name} />
+                        </div>
+                        <h5>{name}</h5>
+                        <small className="client__review">{review}</small>
+                    </SwiperSlide>
+                ))
+            }
             </Swiper>
         </section>
     )
-            }
+}
 
-export default Testimonials
+export default Testimonial;
